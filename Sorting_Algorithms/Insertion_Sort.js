81fa836b18c1fc2f330265a34df4ea4650882e8f
Chaitanya Mistry <ambicanagar17@gmail.com>
2022-08-23 19:53:06 -0400

    Insertion sort implementation done

--- Sorting_Algorithms/Insertion_Sort.js
+++ Sorting_Algorithms/Insertion_Sort.js
@@ -1,9 +1,29 @@
 console.warn('Insertion sort algorithm implementation');
 
-const unsortedArray = [5,2,8,20,50,6];
-                    // 0 1 2  3 4  5
-printArray(unsortedArray);
+const array = [20,11,3,50,45,30,1,2];
 
+console.log('Unsorted Array .. 🤐');
+printArray(array);
+insertionSort(array);
+console.log('Sorted Array .. 😄');
+printArray(array);
+
+// Insertion sort algorithm
+function insertionSort(givenArray){
+
+    for(let i=1;i<givenArray.length;i++){
+        const temp = givenArray[i]; // Current array element
+        let j = i-1; 
+
+        // Shiftin the elements until perfect place for "temp" is found.. 🚂
+        while(j>=0 && givenArray[j] > temp){
+            givenArray[j+1] = givenArray[j];
+            j--;
+        }
+        givenArray[j+1] = temp;
+    }
+
+}
 // To print given array
 function printArray(givenArray){
     givenArray.forEach(element => {
